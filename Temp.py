0df4163c4ef5ccb4f1734e219bc7107bceabaa63
Rohit Magesh <personal.rohit001@gmail.com>
2026-07-13 09:22:22 +0530

    more examples

---
+++ Temp.py
@@ -0,0 +1,10 @@
+My_Array = [[[1,2,3],
+            [4,5,6]],
+            [[7,8,9],
+             [10,11,12]],
+             [[13,14,15],
+              [16,17,18]]
+             ]
+'''It is a list containing nested lists, each nested list 
+consists of 2 lists, each of those 2 lists contains 4 elements'''
+shape = (3,2,4)
---
+++ layer_different_method.py
@@ -0,0 +1,13 @@
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2,0.8,-0.5,1.0],[0.5,-0.91,0.26,-0.5],[-0.26,-0.27,0.17,0.87]]
+biases = [2,3,0.5]
+final_outputs = []
+
+for neuron_weight , neuron_bias in zip(weights,biases):
+    neuron_output = 0
+    for n_inputs , n_weights in zip(inputs , neuron_weight):
+        neuron_output += n_inputs*n_weights
+    neuron_output += neuron_bias
+    final_outputs.append(neuron_output)
+print(final_outputs)
+
